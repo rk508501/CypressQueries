26172b52cccc98d77fa1ec9d8a939e3ff4386f9d
Rushi Kulkarni <ruku508501@gmail.com>
2025-02-23 18:57:35 -0500

    Refactor Cypress tests and configuration for consistency and add Prettier setup

--- cypress/e2e/test/NewTab.cy.js
+++ cypress/e2e/test/NewTab.cy.js
@@ -7,23 +7,24 @@
  * 4. Navigate back to the previous page
  * 5. Verify the page is navigated back to the previous page
  */
-it("Verify the new tab opened", () => {
-  cy.visit("https://knowindia.india.gov.in/districts/");
+it('Verify the new tab opened', () => {
+  cy.visit('https://knowindia.india.gov.in/districts/');
 
   cy.get(
-    ":nth-child(2) > .innr-cont > :nth-child(1) > ul > :nth-child(1) > h3 > a"
+    ':nth-child(2) > .innr-cont > :nth-child(1) > ul > :nth-child(1) > h3 > a'
   )
-    .invoke("removeAttr", "target")
+    .invoke('removeAttr', 'target')
     .click();
 
-    cy.wait(3000)
-
+  cy.wait(3000);
 
   cy.origin('https://andhrapradesh.s3waas.gov.in', () => {
-    cy.url().should('include', 'https://andhrapradesh.s3waas.gov.in')
-    cy.get('body > section.banner-section > div > ul > li.flex-nav-prev > a').click().click() 
-  })
+    cy.url().should('include', 'https://andhrapradesh.s3waas.gov.in');
+    cy.get('body > section.banner-section > div > ul > li.flex-nav-prev > a')
+      .click()
+      .click();
 
-
-
+    //This breaks the test
+    cy.go('back');
+  });
 });
